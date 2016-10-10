377856da1ed63f50ba37cfc0c90f268f1875e489
Niladri Dutta <niladridutta31@gmail.com>
2016-10-10 17:56:40 +0530

    Added 3 articles and their request response

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/articleOne',function(req, res) {
 	res.send('articleOne requested and will be served here');
 });
 
+app.get('/articleTwo',function(req, res) {
+	res.send('articleTwo requested and will be served here');
+});
+
+app.get('/articleTwo',function(req, res) {
+	res.send('articleTwo requested and will be served here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
