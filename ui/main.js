58de0e479e6ec44a6491bc8d208f60fe34f37528
nd7493432401 <niladridutta31@gmail.com>
2016-10-11 17:31:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,9 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'The Heading';
+
+// Move the Image
+var img = document.getElementById('madi');
+img.onClick = function() {
+    img.style.marginLeft = '100px';
+};
