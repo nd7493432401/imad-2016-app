2571d214a02bcff0b78ad0b94b05c0697831a0b1
nd7493432401 <niladridutta31@gmail.com>
2016-10-12 19:53:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ function moveRight () {
     img.style.marginLeft = marginLeft + 'px';
 }
 */
-img.onClick = function () {
+img.onclick = function () {
    //var interval = setInterval(moveRight,10px);
     img.style.marginLeft = '100px';
 };
