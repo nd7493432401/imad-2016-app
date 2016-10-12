6554f2e3784a50ecc0b06074db57767b0c067a96
nd7493432401 <niladridutta31@gmail.com>
2016-10-12 23:33:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,24 @@
 //Counter Code
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function () {
     
-    // Make a request to the counter endpoint
+    // Create a request object
+    var request = new XMLhttpRequest();
     
     // Capture the response and store it in a variable
-    // Render the variable in the correct span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLhttpRequest.DONE) {
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        // Not Done Yet
+    }
+    
+    // Make the request
+    request.open('GET','http://nd7493432401.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
 }
