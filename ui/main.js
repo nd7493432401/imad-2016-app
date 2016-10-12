99891767590470d8c2c2791e1d1b752018062963
nd7493432401 <niladridutta31@gmail.com>
2016-10-12 19:54:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,13 +7,12 @@ element.innerHTML = 'The Heading';
 
 // Move the Image
 var img = document.getElementById('madi');
-/*marginLeft = 0;
+marginLeft = 0;
 function moveRight () {
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
 }
-*/
+
 img.onclick = function () {
-   //var interval = setInterval(moveRight,10px);
-    img.style.marginLeft = '100px';
+   var interval = setInterval(moveRight,10px);
 };
