a1c81c901a3e75c4fea11cc51395a59a0cdc70c7
nd7493432401 <niladridutta31@gmail.com>
2016-10-13 12:16:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,18 +24,32 @@ button.onclick = function () {
 };
 
 // Submit Name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
-    // Make a request to the serverr and send the name
-    // Capture a list of names and render as a list
-    var names = request.responseText;
-    names = JSON.parse(names);
-    var list = '';
-    for(var i=0;i<names.length; i++) {
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    // Create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200){
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0;i<names.length; i++) {
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+    };
+    
+    
+    // Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+
+    request.open('GET','http://nd7493432401.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
